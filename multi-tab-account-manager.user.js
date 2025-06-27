5c80bb730cdc493006bad8b0fb6a216342cd10f9
Minhbeo8 <moimoi1112a@gmail.com>
2025-06-27 11:57:28 +0700

    Update multi-tab-account-manager.user.js

--- multi-tab-account-manager.user.js
+++ multi-tab-account-manager.user.js
@@ -1,22 +1,25 @@
 // ==UserScript==
-// @name         Test Loader Raw
+// @name         Multi-Tab Account Manager for UGPhone (Loader)
 // @namespace    minhbeo8-ugphone
-// @version      1.0.2
-// @description  Test tải code raw trực tiếp
+// @version      1.0.6
+// @description  Quản lý nhiều local ugphone 
+// @author       Minhbeo8(hominz) 
+// @supportURL   https://discord.gg/XK8qsgrF
+// @icon         https://i.postimg.cc/Jhcr8R5L/hominz-png-4.png
 // @match        https://www.ugphone.com/*
+// @match        https://ugphone.com/*
+// @match        https://www.ugphone.com/toc-portal/*
+// @match        https://ugphone.com/toc-portal/*
 // @grant        GM_xmlhttpRequest
+// @grant        GM_info
 // @grant        GM_addStyle
-// @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        GM_deleteValue
 // @grant        GM_listValues
-// @grant        GM_notification
-// @grant        GM_openInTab
-// @grant        GM_registerMenuCommand
-// @grant        GM_unregisterMenuCommand
-// @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
+
 
 (function() {
     'use strict';
