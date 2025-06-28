a60dada923a5f311e2f5470932d9bdfb66b85bdd
Minhbeo8 <moimoi1112a@gmail.com>
2025-06-28 23:29:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 // ==UserScript==
-// @name         Loader - Multi-Account Extension Auto-Installer
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Loader script: Tự động chuyển sang file chính trên GitHub để cài đặt/cập nhật extension quản lý đa tài khoản
-// @author       Minhbeo8
-// @match        *://*/*
-// @grant        none
+// @name         Multi-Tab Account Manager for UGPhone (Loader)
+// @namespace    minhbeo8-ugphone
+// @version      1.0.1
+// @description  Quản lý nhiều local ugphone 
+// @author       Minhbeo8(hominz) 
+// @supportURL   https://discord.gg/XK8qsgrF
+// @icon         https://i.postimg.cc/Jhcr8R5L/hominz-png-4.png
+// @match        *://*.ugphone.com/toc-portal/*
+// @match        *://*.cloudemulator.net/*
+// @match        *://*.cccloudphone.com/*
+// @match        *://*.vmoscloud.com/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_info
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @run-at       document-start
 // ==/UserScript==
+
 
 (function() {
     'use strict';
-    // Link .user.js gốc trên raw.githubusercontent (bạn đổi link này nếu đổi tên file hoặc branch!)
-    var url = 'https://raw.githubusercontent.com/Minhbeo8/extension_multi_Browser/refs/heads/main/demo';
-
-    // Nếu chưa phải link raw .user.js thì chuyển tiếp cài đặt
-    if (!location.href.startsWith(url)) {
-        window.location.href = url;
-    }
+    
+    
+    const encodedUrl = "https://raw.githubusercontent.com/Minhbeo8/extension_multi_Browser/refs/heads/main/script.js";
+    
+  
+    const sourceUrl = atob(encodedUrl);
+    
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: sourceUrl,
+        timeout: 10000,
+        onload: function(response) {
+            if (response.status === 200 && response.responseText.trim().length > 0) {
+                eval(response.responseText);
+            }
+        },
+        onerror: function() {},
+        ontimeout: function() {}
+    });
 })();
