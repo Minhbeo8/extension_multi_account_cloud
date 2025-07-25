d42c43e713d012c5814aadce47d64b54e3be3b72
Minhbeo8 <moimoi1112a@gmail.com>
2025-07-25 17:56:53 +0700

    Update multi-tab-account-manager.user.js

--- multi-tab-account-manager.user.js
+++ multi-tab-account-manager.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name         Multi-Tab Account Manager (Minimal Loader)
+// @name         Multi-Tab Account Manager (Loader)
 // @namespace    http://tampermonkey.net/
-// @version      2.0.0
+// @version      7.0.0
+// @description  Loads the latest version of the Account Manager script.
 // @author       Minhbeo8 (hominz)
 // @icon         https://i.postimg.cc/Jhcr8R5L/hominz-png-4.png
 // @match        https://www.ugphone.com/*
@@ -24,14 +25,18 @@
 (function() {
     'use strict';
 
-    const sourceUrl = 'https://gist.githubusercontent.com/Minhbeo8/eaf509f053d755d739946cb70d70a3fd/raw/MultiTabAccountManager.js';
+    // Chuỗi Base64 chứa URL của Gist
+    const encodedUrl = "aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9NaW5oYmVvOC9lYWY1MDlmMDUzZDc1NWQ3Mzk5NDZjYjcwZDcwYTNmZC9yYXcvMWNhNTJkZjk0ZTk4OWVmNjgwZWNkZjg3NzZhODBmNGY2YjQyZmE0L011bHRJVGFiQWNjb3VudE1hbmFnZXIuanM=";
+
+    // Giải mã URL tại thời điểm chạy
+    const sourceUrl = atob(encodedUrl);
 
     GM_xmlhttpRequest({
         method: "GET",
-        url: `${sourceUrl}?t=${Date.now()}`,
+        url: `${sourceUrl}?t=${Date.now()}`, // Thêm timestamp để đảm bảo luôn tải bản mới
         onload: function(response) {
             if (response.status === 200 && response.responseText) {
-  
+                
                 new Function(
                     'GM_addStyle', 'GM_setValue', 'GM_getValue',
                     'GM_deleteValue', 'GM_listValues', 'GM_openInTab',
@@ -41,6 +46,9 @@
                     GM_deleteValue, GM_listValues, GM_openInTab
                 );
             }
+        },
+        onerror: function(err) {
+            console.error("Account Manager Loader: Failed to load script.", err);
         }
     });
 })();
