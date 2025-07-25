3eeb663ea5a8960b7979cd36a13d19cd3113ba75
Minhbeo8 <moimoi1112a@gmail.com>
2025-07-25 16:59:43 +0700

    Update multi-tab-account-manager.user.js

--- multi-tab-account-manager.user.js
+++ multi-tab-account-manager.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
-// @name         Multi-Tab Account Manager
+// @name         Multi-Tab Account Manager (Minimal Loader)
 // @namespace    http://tampermonkey.net/
-// @version      5.8.0
-// @description  update more cloud phone
-// @author       Minhbeo8 (hominz) 
+// @version      2.0.0
+// @author       Minhbeo8 (hominz)
 // @icon         https://i.postimg.cc/Jhcr8R5L/hominz-png-4.png
 // @match        https://www.ugphone.com/*
 // @match        https://ugphone.com/*
@@ -11,35 +10,37 @@
 // @match        https://cloud.vmoscloud.com/*
 // @match        https://h5.cccloudphone.com/*
 // @match        https://nexus.cccloudphone.com/*
+// @connect      gist.githubusercontent.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 // @grant        GM_listValues
-// @grant        GM_addStyle
 // @grant        GM_openInTab
 // @run-at       document-idle
 // ==/UserScript==
 
-
 (function() {
     'use strict';
-    
-    
-    const encodedUrl = "https://raw.githubusercontent.com/Minhbeo8/extension_multi_Browser/refs/heads/main/extension.js";
-    
-  
-    const sourceUrl = atob(encodedUrl);
-    
+
+    const sourceUrl = 'https://gist.githubusercontent.com/Minhbeo8/eaf509f053d755d739946cb70d70a3fd/raw/MultiTabAccountManager.js';
+
     GM_xmlhttpRequest({
         method: "GET",
-        url: sourceUrl,
-        timeout: 10000,
+        url: `${sourceUrl}?t=${Date.now()}`,
         onload: function(response) {
-            if (response.status === 200 && response.responseText.trim().length > 0) {
-                eval(response.responseText);
+            if (response.status === 200 && response.responseText) {
+  
+                new Function(
+                    'GM_addStyle', 'GM_setValue', 'GM_getValue',
+                    'GM_deleteValue', 'GM_listValues', 'GM_openInTab',
+                    response.responseText
+                )(
+                    GM_addStyle, GM_setValue, GM_getValue,
+                    GM_deleteValue, GM_listValues, GM_openInTab
+                );
             }
-        },
-        onerror: function() {},
-        ontimeout: function() {}
+        }
     });
 })();
