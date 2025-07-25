d17d78fb934050f4d52f61e56e444877971c0ec6
Minhbeo8 <moimoi1112a@gmail.com>
2025-07-25 19:23:57 +0700

    Update multi-tab-account-manager.user.js

--- multi-tab-account-manager.user.js
+++ multi-tab-account-manager.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Multi-Tab Account Manager (Loader)
 // @namespace    http://tampermonkey.net/
-// @version      3.0.0
-// @description  Loads the latest version of the Account Manager script.
+// @version      7.1.0
+// @description  Loads the latest version of the Account Manager script from GitHub repo.
 // @author       Minhbeo8 (hominz)
 // @icon         https://i.postimg.cc/Jhcr8R5L/hominz-png-4.png
 // @match        https://www.ugphone.com/*
@@ -11,7 +11,7 @@
 // @match        https://cloud.vmoscloud.com/*
 // @match        https://h5.cccloudphone.com/*
 // @match        https://nexus.cccloudphone.com/*
-// @connect      gist.githubusercontent.com
+// @connect      raw.githubusercontent.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @grant        GM_setValue
@@ -25,7 +25,9 @@
 (function() {
     'use strict';
 
-    const encodedUrl = "aHR0cHM6Ly9naXN0LmdpdGh1YnVzZXJjb250ZW50LmNvbS9NaW5oYmVvOC9lYWY1MDlmMDUzZDc1NWQ3Mzk5NDZjYjcwZDcwYTNmZC9yYXcvMWNhNTJkZjk0ZTk4OWVmNjgwZWNkZjg3NzZhODBmNGY2YjQyZmE0L011bHRJVGFiQWNjb3VudE1hbmFnZXIuanM=";
+    
+    const encodedUrl = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL01pbmhub2Jlby9tdXRpbF9icm93c2VyX2Nsb3VkL21haW4vZXh0ZW5zaW9uLmpz";
+
     
     const sourceUrl = atob(encodedUrl);
 
@@ -34,7 +36,7 @@
         url: `${sourceUrl}?t=${Date.now()}`,
         onload: function(response) {
             if (response.status === 200 && response.responseText) {
-                
+               
                 new Function(
                     'GM_addStyle', 'GM_setValue', 'GM_getValue',
                     'GM_deleteValue', 'GM_listValues', 'GM_openInTab',
@@ -46,7 +48,7 @@
             }
         },
         onerror: function(err) {
-            console.error("Account Manager Loader: Failed to load script.", err);
+            console.error("Account Manager Loader: Failed to load script from GitHub.", err);
         }
     });
 })();
